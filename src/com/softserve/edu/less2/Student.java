fbef9bf41d680f900fcf06f179937645926a5daf
VasyLviv <vasyl.lviv@gmail.com>
2021-05-04 19:07:00 +0300

    add Student.java from lees2

---
+++ src/com/softserve/edu/less2/Student.java
@@ -0,0 +1,119 @@
+package com.softserve.edu.less2;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Student {
+    private String name;
+    private double rating;
+    private static int studentsNumber;
+    private static double avrRating;
+    private static double totalRating;
+
+    public static void main(String[] args) throws Exception{
+        String studentsName;
+        double studentsRating;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("1st student name?");
+        studentsName = br.readLine();
+        System.out.println("1st student rating");
+        studentsRating = Double.parseDouble(br.readLine());
+        Student Student1st = new Student(studentsName, studentsRating);
+
+        System.out.println("Number of students: " + studentsNumber + " , total rating: " + totalRating + " , average rating: " + avrRating);
+
+
+        Student student2nd = new Student();
+        System.out.println("2nd student name?");
+        studentsName = br.readLine();
+        System.out.println("2nd student rating");
+        studentsRating = Double.parseDouble(br.readLine());
+        student2nd.setName(studentsName);
+        student2nd.setRating(studentsRating);
+
+        System.out.println("Number of students: " + studentsNumber + " , total rating: " + totalRating + " , average rating: " + avrRating);
+
+
+        System.out.println("3rd student name?");
+        studentsName = br.readLine();
+        System.out.println("3rd student rating");
+        studentsRating = Double.parseDouble(br.readLine());
+        Student Student3rd = new Student(studentsName, studentsRating);
+
+        System.out.println("Number of students: " + studentsNumber + " , total rating: " + totalRating + " , average rating: " + avrRating);
+
+
+    }
+
+    public boolean setName(String arg){
+        if (arg.isEmpty()) {
+            return false;
+        }
+        name = arg;
+        return true;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean setRating(double arg){
+        if (arg < 0) {
+            return false;
+        }
+        totalRating -= rating;
+        rating = arg;
+        totalRating += rating;
+        avrRating = totalRating / studentsNumber;
+        return true;
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public boolean betterStudent(Student student1, Student student2){
+        if (student1.rating >= student2.rating)
+            return true;
+        return false;
+    }
+
+    public String toString(){
+        return name + rating;
+    }
+
+    public Student(){
+        studentsNumber++;
+ //       totalRating
+    }
+
+
+    public Student(String argName, double argRating){
+/*        if (argRaing < 0 ) {
+            return false;
+        }
+        totalRating -= rating;
+*/        rating = argRating;
+        name = argName;
+        studentsNumber++;
+        totalRating += rating;
+        avrRating = totalRating / studentsNumber;
+//        return true;
+        //       totalRating
+    }
+
+
+
+
+
+}
+
+/*
+public class Main(String[] args){
+
+}
+*/
