b4f5678a3c3398035f63e1ac616892c62eb76691
2 <2@ggg.com>
2021-05-04 16:24:31 +0200

    Added comments to HW 3

--- src/com/softserve/edu/homework03/App.java
+++ src/com/softserve/edu/homework03/App.java
@@ -6,7 +6,8 @@ public class App {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
+		// Inputting Employees informations
 		System.out.println("Enter space separated name rate hours for first employee");
 		Employee employee0 = new Employee(input.next(), input.nextInt(), input.nextInt());
 		System.out.println("Enter space separated name rate hoursfor second employee");
@@ -14,42 +15,61 @@ public class App {
 		System.out.println("Enter space separated name rate hours for third employee");
 		Employee employee2 = new Employee(input.next(), input.nextInt(), input.nextInt());
 
+		// Printing Employees details
 		System.out.println("Employee details");
 		System.out.println(employee0.toString());
 		System.out.println(employee1.toString());
 		System.out.println(employee2.toString());
-		
-		/*
-		System.out.println("Calculating salaries");
-		System.out.println(employee0.getSalary());
-		System.out.println(employee1.getSalary());
-		System.out.println(employee2.getSalary());
-		*/
-				
-		System.out.println("If you wont to chnage rates enter \" yes \"");
+
+		// Checking is rates for employees require change
+		System.out.println("If you wont to change rates enter \" yes \".");
 		String rateChange = input.next();
-		if (rateChange.equals("yes")|| rateChange.equals("YES")) {
-			System.out.println("Enter new rate for first employee"); employee0.changeRate(input.nextInt());
-			System.out.println("Enter new rate for second employee"); employee1.changeRate(input.nextInt());
-			System.out.println("Enter new rate for third employee"); employee2.changeRate(input.nextInt());	
-		} else {
+
+		if (rateChange.equals("yes") || rateChange.equals("YES")) {
+
+			// Inputting new rates
+			System.out.println("Enter new rate for first employee");
+			employee0.changeRate(input.nextInt());
+			System.out.println("Enter new rate for second employee");
+			employee1.changeRate(input.nextInt());
+			System.out.println("Enter new rate for third employee");
+			employee2.changeRate(input.nextInt());
+
+			// Printing new Employees details
+			System.out.println("Employee details");
+			System.out.println(employee0.toString());
+			System.out.println(employee1.toString());
+			System.out.println(employee2.toString());
+
+			// Calculating total salary with new rates
 			Employee.setTotSum((int) (employee0.getSalary() + employee1.getSalary() + employee2.getSalary()));
-			System.out.println("Total salaries= " + Employee.totSum);
+
+		} else {
+
+			// Calculating total salary with old rates
+			Employee.setTotSum((int) (employee0.getSalary() + employee1.getSalary() + employee2.getSalary()));
+
 		}
-		//Changing rates for all employees
-		
+
+		// Checking if bonuses need to be added
+		System.out.println("If you wont to add bonuses enter \" yes \".");
+		String addBonus = input.next();
+
+		if (addBonus.equals("yes") || addBonus.equals("YES")) {
+
+			// Calculating total salary with bonuses
+			Employee.setTotSum((int) (employee0.getBonuses() + employee1.getBonuses() + employee2.getBonuses()));
+
+		} else {
+
+		}
+
+		System.out.println("Total salary = " + Employee.getTotSum());
+
 		input.close();
 
-		System.out.println("Employee details");
-		System.out.println(employee0.toString());
-		System.out.println(employee1.toString());
-		System.out.println(employee2.toString());
-
-		Employee.setTotSum((int) (employee0.getBonuses()+employee1.getBonuses()+employee2.getBonuses()));
-		System.out.println("Total salary = " + Employee.getTotSum());
 	}
 
-	
 }
 
 class Employee {
