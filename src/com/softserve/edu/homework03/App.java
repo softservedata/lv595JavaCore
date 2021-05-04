19ae640bfa87ac7bb4dd060ce501306d6866ab6e
2 <2@ggg.com>
2021-05-04 10:48:14 +0200

    Removed "Example.java"

---
+++ src/com/softserve/edu/homework03/App.java
@@ -0,0 +1,39 @@
+package com.softserve.edu.homework03;
+
+public class App {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It's correct program");
+		
+	}
+
+}
+class Person{
+	private String name;
+	private String surname;
+	private int age;
+	
+	private String getName() {
+		return name;
+	}
+	private void setName(String name) {
+		this.name = name;
+	}
+	private String getSurname() {
+		return surname;
+	}
+	private void setSurname(String surname) {
+		this.surname = surname;
+	}
+	private int getAge() {
+		return age;
+	}
+	private void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
+
---
+++ src/com/softserve/edu/homework03/Persons.java
@@ -0,0 +1,26 @@
+package com.softserve.edu.homework03;
+
+public class HomeWork3 {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String getFirstName() {
+		return firstName;
+	}
+	private void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	private String getLastName() {
+		return lastName;
+	}
+	private void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	private int getAge() {
+		return age;
+	}
+	private void setAge(int age) {
+		this.age = age;
+	}
+	
+}
