948aa90d990e0bf7b99c907c8c36d629f133f61e
bebebe <hahaha@gmail.com>
2021-05-19 19:56:09 +0300

    Java Inheritance

---
+++ src/com/softserve/edu07inher/A.java
@@ -0,0 +1,24 @@
+package com.softserve.edu07inher;
+
+public class A { // extends Object {
+    private int i = 12;
+
+    public A() {
+        System.out.println("constructor A()");
+    }
+
+    public A(int i) {
+        this.i = i;
+        System.out.println("constructor A(int i)");
+    }
+
+    public int getI() {
+        System.out.println("\tgetI() from A");
+        return i;
+    }
+
+    public static void m() { // Class method
+        System.out.println("\tstatic m() from A");
+    }
+
+}
