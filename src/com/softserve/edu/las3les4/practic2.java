626f9fb5a58cecd85d18c39042809ad5b10863a6
Vasyl Vikhastyy <--list>
2021-05-20 10:40:11 +0300

    + Less4Practic2 transform to enum

--- src/com/softserve/edu/las3les4/practic2.java
+++ src/com/softserve/edu/las3les4/practic2.java
@@ -8,37 +8,54 @@ public class practic2 {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Enter week's day number: ");
+        System.out.println("Enter week's day number [0 - 6] : ");
+        dayOfWeek weeksDay = dayOfWeek.d0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int dayNumber  = Integer.parseInt(br.readLine());
 
         switch (dayNumber) {
             case 0:
-                System.out.println("Неділя - Sunday - 1stDay");
-                break;
             case 1:
-                System.out.println("Неділя - Sunday - 1stDay");
-                break;
             case 2:
-                System.out.println("Понеділок - Monday - 2ndDay");
-                break;
             case 3:
-                System.out.println("Вівторок - Tusday - 3rdDay");
-                break;
             case 4:
-                System.out.println("Середа - Wedneday - 4thDay");
-                break;
             case 5:
-                System.out.println("Четевер - Thersday - 5thDay");
-                break;
             case 6:
-                System.out.println("Пятниця - Friday - 6thDay");
-                break;
-            case 7:
-                System.out.println("Субота - Saturday - 7thDay");
+                System.out.println(weeksDay.valueOf("d"+dayNumber));
                 break;
             default:
                 System.out.println("Wrong in[ut");
+        }
+
+    }
+
+    enum dayOfWeek{
+            d0("Неділя","Sunday","1stDay"),
+            d1("Понеділок","Monday","2ndDay"),
+            d2("Вівторок","Tusday","3rdDay"),
+            d3("Середа","Wedneday","4thDay"),
+            d4("Четевер","Thersday","5thDay"),
+            d5("Пятниця","Friday","6thDay"),
+            d6("Субота","Saturday","7thDay");
+            private String ua;
+            private String en;
+            private String numDay;
+
+
+        dayOfWeek(String ua, String en, String numDay) {
+            this.ua = ua;
+            this.en = en;
+            this.numDay = numDay;
+        }
+
+
+        @Override
+        public String toString() {
+            return "dayOfWeek{" +
+                    "ua='" + ua + '\'' +
+                    ", en='" + en + '\'' +
+                    ", numDay='" + numDay + '\'' +
+                    '}';
         }
     }
 }
