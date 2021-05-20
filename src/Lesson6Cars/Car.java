3d9e7c5608d28bdb55511d7779758ad942b284dd
Murad <nassibov19997@gmail.com>
2021-05-20 21:44:31 +0300

    Read Person

---
+++ src/Lesson6Cars/Car.java
@@ -0,0 +1,45 @@
+package Lesson6Cars;
+
+public abstract class Car {
+    protected String model;
+    protected double maxSpeed;
+    protected int yearOfManufacture;
+
+    public Car(String model, double maxSpeed, int yearOfManufacture) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public abstract void run();
+
+
+    public abstract void stop();
+
+
+}
+
