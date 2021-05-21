6e9eaeed92304075b759bbc2931f1350ef876c06
Vasyl Vikhastyy <--list>
2021-05-21 12:05:39 +0300

    Hw6t1 upd

--- src/com/softserve/edu/less6/Hm6t1.java
+++ src/com/softserve/edu/less6/Hm6t1.java
@@ -3,24 +3,46 @@ package com.softserve.edu.less6;
 public class Hm6t1 {
     public static void main(String[] args) {
         Bird[] birds = {new Penguin(), new Swallow(), new Chicken(), new Eagle()};
-    for (Bird bird : birds)
+    for (Bird bird : birds) {
+        System.out.print(bird);
         bird.fly();
-
+        }
     }
 }
 
-class Eagle extends FlyingBird{}
+class Eagle extends FlyingBird{
+    Eagle(){
+        feathers = true;
+        layEggs = true;
+    }
+}
 
-class Swallow extends FlyingBird{}
+class Swallow extends FlyingBird{
+    Swallow(){
+        feathers = true;
+        layEggs = true;
+    }
+}
 
-class Penguin extends NonFlyingBird{}
+class Penguin extends NonFlyingBird{
+    Penguin(){
+        feathers = false;
+        layEggs = true;
+    }
+}
 
-class Chicken extends NonFlyingBird{}
+class Chicken extends NonFlyingBird{
+    Chicken(){
+            feathers = true;
+            layEggs = true;
+        }
+
+}
 
 class NonFlyingBird extends Bird{
     @Override
     boolean fly() {
-        System.out.println(this.getClass() + " not flies");
+        System.out.println(  " not flies");
         return false;
     }
 }
@@ -29,7 +51,7 @@ class NonFlyingBird extends Bird{
 class FlyingBird extends Bird{
     @Override
     boolean fly() {
-        System.out.println(this.getClass() + " flies");
+        System.out.println( " flies");
         return true;
     }
 }
@@ -40,4 +62,15 @@ abstract class Bird{
     abstract  boolean fly();
 
 
+    @Override
+    public String toString() {
+        if (feathers && layEggs)
+           return this.getClass() + " feathers, layEggs";
+        if (feathers && !layEggs)
+            return this.getClass() + " feathers, no layEggs";
+        if (!feathers && layEggs)
+            return this.getClass() + " no feathers, layEggs";
+
+            return this.getClass() + " no feathers, no layEggs";
+    }
 }
