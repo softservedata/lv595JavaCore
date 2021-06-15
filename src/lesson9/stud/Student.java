c4754878d17ffbb273b2b9559c3382e429c2b65b
Murad <nassibov19997@gmail.com>
2021-06-15 23:58:29 +0300

    Read Person

--- src/lesson9/stud/Student.java
+++ src/lesson9/stud/Student.java
@@ -1,5 +1,9 @@
 package lesson9.stud;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
 public class Student implements Comparable {
     private String name;
     private int course;
@@ -7,6 +11,15 @@ public class Student implements Comparable {
     public Student(String name, int course) {
         this.name = name;
         this.course = course;
+    }
+
+    public String printStudents(List<Student> students, int course) {
+        for (Iterator<Student> iterator = students.iterator(); iterator.hasNext(); ) {
+            Student st = iterator.next();
+            if (st.getCourse() == course)
+                return st.getName();
+        }
+        return " Course ";
     }
 
     public String getName() {
@@ -39,5 +52,17 @@ public class Student implements Comparable {
     public int compareTo(Object o) {
         return 0;
     }
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course && name.equals(student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, course);
+    }
 }
