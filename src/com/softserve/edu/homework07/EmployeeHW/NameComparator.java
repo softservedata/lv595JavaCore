d67d94d279fefae9fc7aace450260868d46f8e0b
2 <2@ggg.com>
2021-06-16 14:28:15 +0100

    Deleted junk

--- src/com/softserve/edu/homework07/EmployeeHW/NameComparator.java
+++
@@ -1,8 +0,0 @@
-package com.softserve.edu.homework07_Employee;
-import java.util.Comparator;
-public class NameComparator implements Comparator<Employee>{
-    @Override
-    public int compare(Employee o1, Employee o2) {
-        return o1.name.compareTo(o2.name);
-    }
-}
--- src/com/softserve/edu/homework07/EmployeeHW/TabComparator.java
+++
@@ -1,8 +0,0 @@
-package com.softserve.edu.homework07_Employee;
-import java.util.Comparator;
-public class TabComparator implements Comparator<Employee>{
-    @Override
-    public int compare(Employee o1, Employee o2) {
-        return o1.tabNumber - o2.tabNumber;
-    }
-}
