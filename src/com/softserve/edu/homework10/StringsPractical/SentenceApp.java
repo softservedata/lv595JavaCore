bd6e2da3229c4e42cd0985e2d87ba7cde6fc9673
2 <2@ggg.com>
2021-07-05 20:59:57 +0100

    added sentence

--- src/com/softserve/edu/homework10/StringsPractical/SentenceApp.java
+++ src/com/softserve/edu/homework10/StringsPractical/SentenceApp.java
@@ -1,8 +1,12 @@
 package com.softserve.edu.homework10.StringsPractical;
 
 public class SentenceApp {
-	public static void main(String[] args) {
+	public static void main(String args) {
+		String[] words = args.split("^[a-zA-Z]*$");
 		
+		for (String word:words) {
+			
+		}
 	}
 
 }
