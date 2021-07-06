a05f8459c96f6c1ba691e670e6927e2425d7d40d
Murad <nassibov19997@gmail.com>
2021-07-06 22:55:40 +0300

    Read Person

--- src/lesson12/HM3/ThreadTwo.java
+++ src/lesson12/HM3/ThreadTwo.java
@@ -5,15 +5,22 @@ import lesson12.hm1.Tred3;
 public class ThreadTwo extends Thread {
     @Override
     public void run() {
-        Thread th3 = new ThreadTree();
+        //Thread th3 = new ThreadTree();
         for (int i = 0; i < 3; i++) {
             System.out.println("Thread 2 Thread ID " + Thread.currentThread().getId());
         }
-        th3.start();
+        //  th3.start();
+        Runnable thread3 = () -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread 3 lambda run Thread ID " + Thread.currentThread().getId());
+            }
+        };
+        new Thread(thread3).start();
         try {
-            th3.join();
+            new Thread(thread3).join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
     }
 }
