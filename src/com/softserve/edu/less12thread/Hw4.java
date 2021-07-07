f6a28f9cbb3fde2df683285edd14186d8b314945
Vasyl Vikhastyy <--list>
2021-07-07 10:58:35 +0300

    less13Hw1

--- src/com/softserve/edu/less12thread/Hw4.java
+++ src/com/softserve/edu/less12thread/Hw4.java
@@ -51,35 +51,16 @@ public class Hw4 {
 //            System.out.println("The longest line is next\n"+fileStrings.get(maxLineLengthId));
             bufferWriter.write("Total lines are : "+ (stringsCount-1)+"\n");
             bufferWriter.write("The longest line is next\n"+fileStrings.get(maxLineLengthId)+"\n");
-
-//        Pattern p = Pattern.compile("\\b(\\w*\\b)*\\w*Name\\w*\\b(\\w*)*\\b");
-//        Pattern p = Pattern.compile("Name:\\s*(\\w*)\\s*(\\w*)\\s*(\\w*)\\b");
-//            Pattern p = Pattern.compile("Name:(\\s*(\\w+))*"); // \\s*(\\w*)\\s*(\\w*)\\s*(\\w*)\\b");
-//           Pattern p = Pattern.compile("Name:(.*)\\b");
-//        Pattern p = Pattern.compile("((\\w*\\s)*(\\w*Name:\\w*){1}(\\s\\wVas\\w*)*\\b)");
            Matcher m;
            boolean someFound = false;
-//           Pattern p = Pattern.compile("Name([:, ]+(\\w+))+\\s*");
            for(String curr : fileStrings){
                // Name proccessing!! -------------------------------------
                Pattern p = Pattern.compile("my\\s*name\\s*:");
-//            System.out.println(curr);
                m = p.matcher(curr.toLowerCase());
-//               m.reset();
                int i = 1;
                if (m.find())
                {
                    System.out.println("Name: - found");
-/*                  String[] names = m.group(0).replace("  "," ")
-                          .replace(": ",":")
-                          .replace(", ",",")
-                          .split(":|,| ");
-                  for (i = 1 ; i<names.length; i++ )
-                {
-                    System.out.println(names[i]);
-                }
-*/
-//                 p = Pattern.compile("[:, ]\\w+");
                    p = Pattern.compile("\\b\\w+");
                    curr = curr.substring(curr.lastIndexOf(':',curr.length()));
                    System.out.println(curr);
@@ -92,15 +73,12 @@ public class Hw4 {
                 }
                } else   // BirthDay processing ---------
                {
-//                   System.out.println("looking BirtDay!!");
-//                   System.out.println(curr.toLowerCase());
                    p = Pattern.compile("\\s*birth\\s*date\\s*:");
                    m = p.matcher(curr.toLowerCase());
                m.reset();
                   if (m.find())
                    {
                        System.out.println("BirthDay: - found");
-//                       p = Pattern.compile("\\d+[., ]+\\d+[., ]+\\d+");
                        p = Pattern.compile("\\d+");
                        curr = curr.substring(curr.lastIndexOf(':',curr.length()));
                        System.out.println("current string: " +curr);
@@ -110,16 +88,11 @@ public class Hw4 {
                        while (m.find())
                        {
                            System.out.println("next:"+curr.substring(m.start(),m.end()));
-//                    System.out.print("ssome found!!! - groups = " + m.groupCount());
-//                    System.out.println("Group#"+i+" :"+m.group(i++)+";");
                            bufferWriter.write("BD" + i++ +":"+curr.substring(m.start(),m.end())+",");
                        }
                    }
 
                }
-//                System.out.println(m.group(1));
-//                System.out.println(curr.substring(m.end(1)));
-//              }
            }
            bufferWriter.close();
         }
