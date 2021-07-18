447b9e187814437c3fdd5241a8a43f832c73f823
Murad <nassibov19997@gmail.com>
2021-07-18 18:56:47 +0300

    Added task jdcb

--- src/main/java/com/softserve/lesson15jdbc/Task.java
+++ src/main/java/com/softserve/lesson15jdbc/Task.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.*;
 
+
 public class Task {
     private static Connection connect = null;
     private static final String username = "root";
